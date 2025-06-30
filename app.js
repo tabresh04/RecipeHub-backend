08c227160efe8cc9bf8a5fd3a9a036b8c41bd4fc
Shaikh Tabresh <159866113+tabresh04@users.noreply.github.com>
2025-06-30 07:01:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://recipehub-frontend-f6ep.onrender.com', 
   credentials: true    
 }));
 
